5eed951cca5b14ce8529fe1e5855417b4973d3f2
jorge <jorgemalaquia@gmail.com>
2022-08-10 23:43:43 -0300

    router hashtag

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import GlobalStyle from '../assets/styles/GlobalStyle';
-import TimeLine from '../pages/timeline/TimelineScreen';
-import UserContext from './contexts/UserContext';
-import LoginScreen from './pages/LoginScreen/LoginScreen';
-import RegisterScreen from './pages/RegisterScreen/RegisterScreen';
+import GlobalStyle from '../../assets/styles/GlobalStyle.js';
+import TimeLine from '../pages/timeline/TimelineScreen.jsx';
+import UserContext from '../contexts/UserContext.js';
+import LoginScreen from '../pages/LoginScreen/LoginScreen.jsx';
+import RegisterScreen from '../pages/RegisterScreen/RegisterScreen.jsx';
 
 function App() {
   return (
@@ -15,6 +15,8 @@ function App() {
           <Route path={'/'} element={<LoginScreen />} />
           <Route path={'/sign-up'} element={<RegisterScreen />} />
           <Route path={'/timeline'} element={<TimeLine />} />
+          <Route path={'/hashtag/:hashtag'}/>
+
         </Routes>
       </BrowserRouter >
     </UserContext.Provider>
